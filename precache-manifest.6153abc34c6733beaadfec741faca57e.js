cd78680b27a5afe224d8489f199c11be687d20d1
randomicons <srirambhat7@gmail.com>
2019-04-22 00:30:25 -0500

    Updates

---
+++ precache-manifest.6153abc34c6733beaadfec741faca57e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d56ef6ac9adb18a6ff3",
+    "url": "/static/css/main.73ab9dfa.chunk.css"
+  },
+  {
+    "revision": "6d56ef6ac9adb18a6ff3",
+    "url": "/static/js/main.f8afbc25.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cb3b52f6eac7591a371211d5437780c5",
+    "url": "/index.html"
+  }
+];
