2e08e7f5207c2dd3ce21a4c24fa99295c7235ca6
randomicons <srirambhat7@gmail.com>
2019-06-12 18:05:52 -0500

    transitions work, finally!

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,7 @@ import React, {Component} from 'react'
 
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {faChevronDown} from '@fortawesome/free-solid-svg-icons'
-import {TweenMax} from "gsap"
-import {Transition} from "react-transition-group"
 import TransitionSection from "./TransitionSection"
-import styles from './App.module.sass'
 
 library.add(faChevronDown)
 
@@ -24,70 +21,31 @@ class App extends Component {
   render() {
     return (
       <main>
-        <div className={styles.section_container} style={{zIndex: 2}}>
-          <TransitionSection background={"#ffcc60"} show={this.checkPage(0)} arrowAction={
-            () => this.setState({page: this.state.page + 1})}
-          >
-            <div style={{background: "white"}}>
-              <p>A bird in the hand is worth 2 in the bush</p>
-              <h1>Sriram Bhat</h1>
-              <span>Web developer. </span>
-              <span>Data Something.</span>
-              <span>Confused.</span>
-            </div>
-          </TransitionSection>
-        </div>
-        <div className={styles.section_container}>
-          <section>
-            <TransitionSection background={"#ffd5ff"} show={this.checkPage(1)} arrowAction={
-              () => this.setState({page: this.state.page + 1})}
-            >
-              <div style={{background: "white"}}>
-                <p>A bird in the hand is worth 2 in the bush</p>
-                <h1>Sriram Bhat</h1>
-                <span>Web developer. </span>
-                <span>Data Something.</span>
-                <span>Confused.</span>
-              </div>
-            </TransitionSection>
-          </section>
-        </div>
-        {/*{this.checkPage(1) &&*/}
-        {/*<Transition*/}
-        {/*  appear*/}
-        {/*  // onEnter={node => TweenMax.set(node, {y: -1000})}*/}
-        {/*  addEndListener={(node, done) => {*/}
-        {/*    const contentDiv = node.childNodes[0]*/}
-        {/*    const speed = .8*/}
-        {/*    if (this.checkPage(1)) {*/}
-        {/*      // TweenMax.to(node, speed, {y: 0, onComplete: done,})*/}
-        {/*    } else {*/}
-        {/*      TweenMax.set(node, {y: "-100%"})*/}
-        {/*      TweenMax.to(node, speed, {y: "-200vh", ease: Expo.easeInOut, onComplete: done,})*/}
-        {/*      TweenMax.to(contentDiv, speed, {y: "100vh", ease: Expo.easeInOut})*/}
-        {/*      // TweenMax.set(node, {y: "100vh"})*/}
-        {/*      // done()*/}
-        {/*    }*/}
-        {/*  }}*/}
-        {/*>*/}
-        {/*  <section className={styles.content}>*/}
-        {/*    <div className={styles.down}>*/}
-        {/*      <span><FontAwesomeIcon onClick={() => this.setState({page: this.state.page + 1})}*/}
-        {/*                             icon={"chevron-down"}/></span>*/}
-        {/*    </div>*/}
-        {/*  </section>*/}
-        {/*</Transition>*/}
+        <TransitionSection background={"#d5ffff"} zIndex={2} show={this.checkPage(0)} arrowAction={
+          () => this.setState({page: this.state.page + 1})}
+        >
+          <div style={{background: "white"}}>
+            <p>A bird in the hand is worth 2 in the bush</p>
+            <h1>Sriram Bhat</h1>
+          </div>
+        </TransitionSection>
+        <TransitionSection background={"#ffd5ff"} zIndex={1} show={this.checkPage(1)} arrowAction={
+          () => this.setState({page: this.state.page + 1})}>
+          <div style={{background: "white"}}>
+            <p>A bird in the hand is worth 2 in the bush</p>
+            <h1>Sriram Bhat</h1>
+          </div>
+        </TransitionSection>
+        {/*{*/}
+        {/*  <Transition*/}
+        {/*    show={this.checkPage(1)}*/}
+        {/*    appear mountOnEnter unmountOnExit*/}
+        {/*    onEnter={node => TweenMax.set(node, {y: "-100vh"})}*/}
+        {/*    onEntered={node => TweenMax.set(node, {y: "0vh"})}*/}
+        {/*  >*/}
+        {/*    <section style={{background: "#f00", height: "100vh"}}><p>Some nonsense</p></section>*/}
+        {/*  </Transition>*/}
         {/*}*/}
-        {
-          <Transition
-            show={this.checkPage(1)}
-            appear mountOnEnter unmountOnExit
-            onEnter={node => TweenMax.set(node, {y: "-100vh"})}
-            onEntered={node => TweenMax.set(node, {y: "0vh"})}
-          >
-            <section style={{background: "#f00", height: "100vh"}}><p>Some nonsense</p></section>
-          </Transition>
-        }
       </main>
     )
   }
--- src/components/TransitionSection.js
+++ src/components/TransitionSection.js
@@ -3,32 +3,72 @@ import {Transition} from "react-transition-group"
 import {Expo, TweenMax} from "gsap"
 import styles from "./TransitionSection.module.sass"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types"
 
 const transSpeed = 1
+const rotAngle = 35
 export default class TransitionSection extends Component {
+
+  constructor(props) {
+    super(props)
+
+  }
+
+  cos(deg) {
+    return Math.abs(Math.cos(deg * Math.PI / 180))
+  }
+
+  sin(deg) {
+    return Math.abs(Math.sin(deg * Math.PI / 180))
+  }
 
   render() {
     return (
       <Transition
         in={this.props.show}
-        appear unmountOnExit
+        appear mountOnEnter unmountOnExit
         addEndListener={(node, done) => {
-          const contentDiv = node.childNodes[0]
-          if (this.props.show) {
-          } else {
-            // TweenMax.set(node, {y: "-100%"})
-            TweenMax.to(node, transSpeed, {y: "-100%", ease: Expo.easeInOut, onComplete: done,})
-            TweenMax.to(contentDiv, transSpeed, {y: "100vh", ease: Expo.easeInOut})
+          const rotation_div = node.childNodes[0]
+          const move_div = rotation_div.childNodes[0]
+          const contentDiv = move_div.childNodes[0]
+
+          if (this.props.show && this.props.doInitAnim) {
+            TweenMax.to(node, transSpeed, {startAt: {y: "-100%"}, y: "100vh", ease: Expo.easeInOut, onComplete: done})
+
+          } else if (this.props.show) {
+            console.log(rotation_div, move_div, contentDiv)
+            console.log(move_div.offsetWidth, move_div.offsetHeight)
+
+            move_div.style.width = `calc(100vw * ${Math.abs(Math.cos(rotAngle * Math.PI / 180))} + 100vh * ${Math.abs(Math.sin(rotAngle * Math.PI / 180))})`
+            move_div.style.height = `calc(100vw * ${Math.abs(Math.sin(rotAngle * Math.PI / 180))} + 100vh * ${Math.abs(Math.cos(rotAngle * Math.PI / 180))})`
+            console.log(move_div.offsetWidth, move_div.offsetHeight)
+            move_div.style.transform = `translate(calc(-1 * (100vw - ${move_div.offsetWidth}) / 2), calc(-1 * (100vh - ${move_div.offsetHeight}) / 2)`
+            rotation_div.style.transform = `rotate3d(0,0,1,${rotAngle}deg)`
+            TweenMax.set(contentDiv, {rotation: -1 * rotAngle})
+          } else if (!this.props.show) {
+            TweenMax.to(move_div, transSpeed, {y: "-100%", ease: Expo.linear, onComplete: done,})
+            TweenMax.to(contentDiv, transSpeed, {y: "100%", ease: Expo.linear})
           }
         }}
       >
-        <section className={styles.content} style={{background: this.props.background}}>
-          {this.props.children}
-          <div className={styles.down}>
-            <span><FontAwesomeIcon onClick={this.props.arrowAction} icon={"chevron-down"}/></span>
+        <div className={styles.container}>
+          <div className={styles.rotation_div} style={{zIndex: this.props.zIndex}}>
+            <div className={styles.move_div} style={{
+              background: this.props.background,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center"
+            }}>
+              <div className={styles.content}>
+                {this.props.children}
+                <div className={styles.dot}></div>
+                <div className={styles.down}>
+                  <span><FontAwesomeIcon onClick={this.props.arrowAction} icon={"chevron-down"}/></span>
+                </div>
+              </div>
+            </div>
           </div>
-        </section>
+        </div>
       </Transition>
     )
   }
@@ -37,5 +77,6 @@ export default class TransitionSection extends Component {
 TransitionSection.propTypes = {
   show: PropTypes.bool.isRequired,
   background: PropTypes.string.isRequired,
-  arrowAction: PropTypes.func.isRequired
+  arrowAction: PropTypes.func.isRequired,
+  doInitAnim: PropTypes.bool
 }
